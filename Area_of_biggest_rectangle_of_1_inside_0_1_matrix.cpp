78efa68ea551aa7fd5ca7c350ba62cb3cc75ca9c
Habibur Rahman <habibrahman6093@gmail.com>
2018-07-26 13:02:18 +0600

    Update Area_of_biggest_rectangle_of_1_inside_0_1_matrix.cpp

--- Area_of_biggest_rectangle_of_1_inside_0_1_matrix.cpp
+++ Area_of_biggest_rectangle_of_1_inside_0_1_matrix.cpp
@@ -37,10 +37,10 @@ int main()
   
   for(int x=0;x<5;x++)
    {
-     
+     int arr[100]={};
      for(int y=0;y<8;y++)
      {  
-         //cout<<matrix[x][y]<<endl;
+         //cout<<"index: "<<y+1<<"   "<<matrix[x][y]<<endl;
          if((index.empty() || index.top().first<=matrix[x][y]) && matrix[x][y]>0)
          {
          	index.push(make_pair(matrix[x][y],y));
@@ -48,25 +48,29 @@ int main()
          }
          else if(!index.empty() && matrix[x][y]<index.top().first)
          {
-         	int count=index.top().second+1,last_index;
+         	int count=index.top().second+1,last_index=0,cnt=0;
+         	
          	while(!index.empty() && matrix[x][y]<index.top().first)
-         	{
-         	    last_index=index.top().second;
-         	    temp=(count-index.top().second)*index.top().first;
+         	 {
+         	    if(index.top().second>last_index)
+         	     last_index=index.top().second;
+         	     
+         	    temp=(count-index.top().second)*index.top().first+arr[index.top().second];
          	    index.pop();
-         	    //cout<<"poped"<<endl;
-         	    //cout<<"result: "<<temp<<endl;
-         	    
+         	 
+         	    cnt++;
          	    if(temp>final)
          	     final=temp;
          	     
-
-         	}
+         	    // cout<<"poped"<<endl;
+         	     
+              }
          	
          	if(matrix[x][y] > 0)
          	 {
-         	   index.push(make_pair(matrix[x][y],last_index));
-         	   //cout<<"pushed  "<<index.top().second<<endl;
+         	   index.push(make_pair(matrix[x][y],y+1));
+         	   arr[index.top().second]=cnt*matrix[x][y];
+         	   //cout<<"pushed  "<<index.top().second<<"  count is: "<<cnt*matrix[x][y] <<endl;
          	 }
          }
      	
@@ -74,20 +78,24 @@ int main()
      
      if(!index.empty())
      {
-         	int count=index.top().second+1,last_index;
+         	int count=index.top().second+1,last_index,cnt=0;
+         	
          	while(!index.empty())
          	{
-         	    last_index=index.top().second;	 
-         	    temp=(count-index.top().second)*index.top().first;
+         	    if(index.top().second>last_index)
+         	     last_index=index.top().second;
+         	      
+         	    temp=(count-index.top().second)*index.top().first+arr[index.top().second];
          	    index.pop();
          	    //cout<<"popedes"<<endl;
          	    
          	    if(temp>final)
          	     final=temp;
-         	     
 
          	}
      }
+     
+     //cout<<"final is: :::::: "<<final<<endl;
  
    }
    
